a9fba7c30b5d51ad6c683d4f0ba39e8ec123351a
bogda <bodzanzar@gmail.com>
2023-05-06 00:19:58 +0300

    Fixing bug of cutting words.

--- src/main/java/com/example/test/server/mappers/SentDtoMapper.java
+++ src/main/java/com/example/test/server/mappers/SentDtoMapper.java
@@ -3,12 +3,16 @@ package com.example.test.server.mappers;
 import com.example.test.client.dto.SentDTO;
 import com.example.test.server.dto.IncomingMessageDTO;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SentDtoMapper {
     public SentDTO transformToSent(IncomingMessageDTO incomingMessage) {
         SentDTO sentDTO = new SentDTO();
-        sentDTO.setTexts(List.of(incomingMessage.getText().split(" ")));
+        List<String> texts = new java.util.ArrayList<>(List.of(incomingMessage.getText().split(" ")));
+        texts.removeIf(String::isEmpty);
+        sentDTO.setTexts(texts);
         sentDTO.setSourceLanguageCode(incomingMessage.getSourceLanguageCode());
         sentDTO.setTargetLanguageCode(incomingMessage.getTargetLanguageCode());
         return sentDTO;
